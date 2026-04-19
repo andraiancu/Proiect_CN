0f89c21052cf6e334ca29616ccd1a2dad3bea05a
madalina313 <termuremadalina@yahoo.ro>
2026-04-19 15:59:32 +0300

    Add Verilog module for 1-bit full adder

--- src/common/fac.v
+++ src/common/fac.v
@@ -1,1 +1,22 @@
+// fac.v - Full Adder pe 1 bit
+// Intrari: a, b, cin
+// Iesiri:  sum, cout 
 
+module fac(
+
+    input  a,
+    input  b,
+    input  cin,
+    output sum,
+    output cout
+
+);
+
+    wire w1, w2, w3;
+    xor G1(w1, a, b);
+    xor G2(sum, w1, cin);
+    and G3(w2, a, b);
+    and G4(w3, w1, cin);
+    or  G5(cout, w2, w3);
+
+endmodule
