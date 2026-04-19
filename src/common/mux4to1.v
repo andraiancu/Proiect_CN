10d2c2b64c64b3c2603230ecb187a34708b3faba
andraiancu <77541338+andraiancu@users.noreply.github.com>
2026-04-19 23:55:33 +0300

    Implement 4-to-1 multiplexer for 16-bit inputs

--- src/common/mux4to1.v
+++ src/common/mux4to1.v
@@ -1,1 +1,20 @@
+module mux4to1_16bit(
+    input [15:0] in0,    // Rezultat Adunare 
+    input [15:0] in1,    // Rezultat Scadere 
+    input [15:0] in2,    // Rezultat Inmultire
+    input [15:0] in3,    // Rezultat Impartire 
+  input [1:0]  sel,    // selectia
+    output [15:0] out
+);
 
+    always @(*) begin
+        case (sel)
+            2'b00:   out = in0; //daca sel e 00 iesirea primeste in0
+            2'b01:   out = in1; // tot asa
+            2'b10:   out = in2; 
+            2'b11:   out = in3; 
+            default: out = 16'b0;// in caz ca ceva nu merge si intrarea e z sau ceva
+        endcase
+    end
+
+endmodule
